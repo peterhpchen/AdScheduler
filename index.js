db3407ea2845142d1dd12241213d622f889b0ccd
peter_hp_chen <peter_hp_chen@mail.gss.com.tw>
2017-11-14 21:52:08 +0800

    把日期相關的處理移到utility中

--- index.js
+++ index.js
@@ -1,48 +1,17 @@
 'use strict';
 
 var inquirer = require('inquirer');
-var moment = require('moment');
 var extend = require('node.extend');
 
 var scheduler = require('./lib/scheduler');
 var arranger = require('./lib/arranger');
 var exporter = require('./lib/exporter');
-var inquirerValidator = require('./lib/inquirer/validator');
-var inquirerChoice = require('./lib/inquirer/choice');
 var inquirerQuestion = require('./lib/inquirer/question');
 var utility= require('./lib/infrastructure/utility');
 
 var result = {};
 
-
 console.log('Start to create an Ad Schedule\n');
-
-function getDateByDayOfWeek(startDate, endDate, dayOfWeek) {
-    var week = utility.getWeekInfo(startDate, endDate);
-    var result = [];
-
-    var i = 0;
-    for (; i < dayOfWeek.length; i++) {
-        var day = week[dayOfWeek[i]];
-        if (!day.length) continue;
-        result = result.concat(day);
-    }
-
-    return result;
-}
-
-function getSelectedDate(answers) {
-    var week = answers.week;
-    var startDate = answers.startDate;
-    var endDate = answers.endDate;
-    var weekday = [1, 2, 3, 4, 5];
-    var weekend = [0, 6];
-
-    if (week === 'weekday') return getDateByDayOfWeek(startDate, endDate, weekday);
-    if (week === 'weekend') return getDateByDayOfWeek(startDate, endDate, weekend);
-    if (week === 'specificDay') return getDateByDayOfWeek(startDate, endDate, answers.playDay);
-    return answers.playDate;
-}
 
 function askTimePeriodQuestion(answers) {
     result = extend(result, answers);
@@ -68,7 +37,7 @@ function askTimeQuestion(answers) {
     if (answers.time === 'workTime') return inquirer.prompt([]);
     if (answers.time === 'specificTime') return inquirer.prompt(inquirerQuestion.timeQuestions());
     if (answers.time === 'specificTimeOnDate') {
-        var dates = getSelectedDate(answers).sort();
+        var dates = utility.getSelectedDate(answers).sort();
 
         var timeQuestions = [];
         dates.forEach((date) => {
